0328ba66be366139bdf6906e76e9568201b0cc7e
gythwulf <gythwulf@gmail.com>
2022-08-13 11:15:14 -0700

    Update enUS.lua
    Added locales

--- GGSocialState2/Locales/enUS.lua
+++ GGSocialState2/Locales/enUS.lua
@@ -43,7 +43,8 @@ L["Show or hide the guild name."] = true
 L["Show or hide the hint line under tooltip."] = true
 L["Split Real ID and Normal Friends"] = true
 L["Split Real ID and Normal Friends on the LDB"] = true
+L["Hide InApp Friends"] = true
+L["Hide RealID friends that are \"In App\""] = true
 L["The tooltip will hide when not hovered over for this (default: 0.25)"] = true
 L["to open the friends panel."] = true
 L["to open the guild panel."] = true
-
